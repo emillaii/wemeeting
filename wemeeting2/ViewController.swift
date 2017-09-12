422fd4536e58dc2484ee907d7c8f75c0df94cc69
emil <emillai@onwardsmg.com>
2017-09-12 22:47:56 +0800

    First Commit

---
+++ wemeeting2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wemeeting2
+//
+//  Created by Lai kwok tai on 12/9/2017.
+//  Copyright © 2017 Lai kwok tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
